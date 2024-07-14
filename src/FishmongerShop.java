6eb5d8bba70602761aef7410003bb45efef79f87
clairejcowan <clairejcowan@outlook.com>
2024-07-15 00:02:48 +0100

    Task 2: Modify the shop’s behavior so that customers wait outside when the shop is full instead of leaving, ensure the fishmonger sleeps when no customers are present and wakes up upon a new customer's arrival, and implement detailed console logging with delays for better readability and progress tracking.

--- src/FishmongerShop.java
+++ src/FishmongerShop.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class FishmongerShop {
     private ArrayList<Object> w;
@@ -9,26 +10,67 @@ public class FishmongerShop {
     }
 
     public synchronized void enter(Object item) {
-        if (w.size() == MAX_CAPACITY)
-            System.out.println("Maximum capacity reached. Customer leaving.");
-        else {
-            System.out.println("New Customer. Shop had " + w.size() + " customers waiting.");
-            w.add(item);
-            if (w.size() == 1)
-                System.out.println("Customer has rang the bell. Fishmonger is awake.");
+        while (w.size() == MAX_CAPACITY) {
+            System.out.println("Shop is full. Customer waiting outside...");
+            try{
+                //block enter
+                wait();
+            }
+            catch(InterruptedException e){
+                System.out.println("Enter Interrupt Error: " + e);
+            }
         }
+
+        //log new customer
+        System.out.println("New Customer. Shop had " + w.size() + " customers waiting.");
+        w.add(item);
+
+        try{
+            //random execution delay
+            Thread.sleep(new Random().nextLong(500));
+        }
+        catch (InterruptedException e){
+            System.out.println("Enter Sleep Interrupt Error: " + e);
+        }
+
+        //notify serveCustomer
+        notify();
     }
 
     public synchronized Object serveCustomer() {
-        Object item = null;
+        boolean isResting = false;
 
-        if (w.size() == 0)
+        while (w.size() == 0) {
             System.out.println("Shop is empty. Fishmonger resting...");
-        else {
-            System.out.println("Fishmonger serving. Shop had " + w.size() + " customers waiting. ");
-            item = w.get(0);
-            w.remove(0);
+            isResting = true;
+            try {
+                //block serveCustomer
+                wait();
+            }
+            catch (InterruptedException e){
+                System.out.println("Serve Customer Interrupt Error: " + e);
+            }
         }
+
+        //wake up fishmonger
+        if(isResting)
+            System.out.println("Customer rang the bell. Fishmonger is awake.");
+
+        //serve customer
+        System.out.println("Fishmonger serving. Shop had " + w.size() + " customers waiting.");
+        Object item = w.get(0);
+        w.remove(0);
+
+        try{
+            //Random execution delay
+            Thread.sleep(new Random().nextLong(500));
+        }
+        catch (InterruptedException e){
+            System.out.println("Serve Customer Sleep Interrupt Error: " + e);
+        }
+
+        //notify enter
+        notify();
         return item;
     }
 
